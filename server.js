bd14f7694609925de259902687d32a49d796cdbb
Paige Knowles <paknowles@student.fullsail.edu>
2023-09-30 19:46:41 -0400

    Second commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     const { headers, url, method } = req; 
-    console.log(headers, url, method);
+    // console.log(headers, url, method);
     res.end();
 });
 
